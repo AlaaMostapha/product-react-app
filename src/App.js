a6ed2ceb7071dcf5a6afcc49ffab37a23dc9e7d6
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-04 01:47:19 +0200

    add dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.scss';
-
+import  CreateButton from './components/Btn/Btn';
+import  MediaCard from './components/Card/Card';
+import FadeMenu from './components/Menu/Menu';
 function App() {
   return (
     <div className="App">
+      <CreateButton color="primary" text="search" href="#"/>
+      <CreateButton color="secondary"/>
+      <FadeMenu/>
+      <MediaCard title="lizard" discription="lllizzzard" img="https://hbr.org/resources/images/article_assets/2020/04/Apr20_07_1162572100-768x432.jpg"/>
+      <MediaCard title="horse" discription="lllizzzard" img="https://www.fiftyfiveandfive.com/wp-content/uploads/2019/06/header-2-2000x500.png"/>
+
       <header className="App-header">
       </header>
     </div>
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import './Menu.scss'
+
+export default function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+       <img src="https://i.vippng.com/png/small/355-3554387_create-digital-profile-icon-blue-profile-icon-png.png" 
+        alt=""
+        className="profileImg"/>
+      </Button>
+      <Menu
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>View/ Edit Profile </MenuItem>
+        <MenuItem onClick={handleClose}>Sign Out </MenuItem>
+      </Menu>
+    </div>
+  );
+}
