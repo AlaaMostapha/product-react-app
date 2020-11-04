00079de65bebfbcf36652c26df3e0ab22237eb86
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-04 16:21:40 +0200

    structure app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.scss';
 import  CreateButton from './components/Btn/Btn';
-import  MediaCard from './components/Card/Card';
-import FadeMenu from './components/Menu/Menu';
-import MenuAppBar from './containers/header/Header';
+import Header from './containers/header/Header';
+import ProductList from './pages/productList/productList'
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
-import {getProducts,showProducts} from '../src/redux/actions/actions';
-import {connect} from 'react-redux';
-import * as actions from './redux/actions/actions';
-  const state = store.getState();
+// import {getProducts,showProducts} from '../src/redux/actions/actions';
+// import {connect} from 'react-redux';
+// import * as actions from './redux/actions/actions';
+  // const state = store.getState();
 function App() {
   
   return (
@@ -19,12 +18,8 @@ function App() {
       {console.log(store.getState)}
 {console.log(state)} */}
       <div className="App">
-        <MenuAppBar/>
-        <CreateButton color="primary" text="search" href="#"/>
-        <CreateButton color="secondary"/>
-        <FadeMenu/>
-        <MediaCard title="lizard" discription="lllizzzard" img="https://hbr.org/resources/images/article_assets/2020/04/Apr20_07_1162572100-768x432.jpg"/>
-        <MediaCard title="horse" discription="lllizzzard" img="https://www.fiftyfiveandfive.com/wp-content/uploads/2019/06/header-2-2000x500.png"/>
+        <Header/>
+        <ProductList/>
       </div>
     </Provider>
   );
---
+++ src/axios/handlers/index.js
@@ -0,0 +1,10 @@
+export const handleRequest= (request)=>{
+    console.log("interceptor request")
+    console.log(request)
+}
+export const handleResponse= (reponse)=>{
+    console.log(reponse)
+}
+export const handleError= (error)=>{
+    console.log(error)
+}
