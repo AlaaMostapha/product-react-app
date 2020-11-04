9a216ec7469a06ab80637319100e914c3a1631a5
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-04 22:35:34 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import './App.scss';
-import  CreateButton from './components/Btn/Btn';
 import Header from './containers/header/Header';
 import ProductList from './pages/productList/productList'
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
-// import {getProducts,showProducts} from '../src/redux/actions/actions';
-// import {connect} from 'react-redux';
-// import * as actions from './redux/actions/actions';
-  // const state = store.getState();
+import ProductDetails from './pages/productDetails/productDetails'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   
   return (
     <Provider store={store}>
-      {/* {console.log(state)}
-      {console.log(store.dispatch(getProducts()))}
-      {console.log(store.dispatch(showProducts()))}
-      {console.log(store.getState)}
-{console.log(state)} */}
       <div className="App">
-        <Header/>
-        <ProductList/>
+           <Header/>
+        <Router>
+    
+            <Route exact path="/products" component={ProductList}/>
+          <Route   exact path="/products/:productId" component={ProductDetails}/>
+          
+     
+        </Router>
+     
       </div>
     </Provider>
   );
 }
 
-// function mapDispatchToProps(dispatch){
-//   return{
-//    getProducts : ()=>dispatch(actions.getProducts())
-//   }
-// } 
-// export default connect(null,mapDispatchToProps)(App); 
 
 export default App;
