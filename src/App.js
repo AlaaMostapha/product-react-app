5912386249ab8e531051ffea92d2695fb4ba35a5
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-05 18:46:35 +0200

    add new routes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import Header from './containers/header/Header';
 import ProductList from './pages/productList/productList'
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
-import ProductDetails from './pages/productDetails/productDetails'
+import ProductDetails from './pages/productDetails/productDetails';
+import ReviewOrder from './pages/ReviewOrder/ReviewOrder';
+import OrderNow from './pages/UserFormOrderNow/OrderNow';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 function App() {
   
   return (
     <Provider store={store}>
       <div className="App">
-           <Header/>
+        <Header/>
         <Router>
-            <Route exact path={["/", "/products"]} component={ProductList}/>
+          <Route exact path={["/", "/products"]} component={ProductList}/>
           <Route   exact path="/products/:productId" component={ProductDetails}/>
-          
-     
+          <Route exact path="/ReviewOrder" component={ReviewOrder}/>
+          <Route exact path="/OrderNow" component={OrderNow}/>
         </Router>
-     
       </div>
     </Provider>
   );
