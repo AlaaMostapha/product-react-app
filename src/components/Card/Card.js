a40fbadbf366b6c87e760b1a37ad53b8279d1c4e
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-05 18:46:58 +0200

    add btn component to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "./Card.scss";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import  CreateButton from '../../components/Btn/Btn';
 
 const useStyles = makeStyles({
   root: {
@@ -27,14 +29,15 @@ export default function MediaCard({title,discription,img,alt,onClick}) {
           title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant="h5" component="h2" className="limitCharacters">
             {title}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body2" color="textSecondary" component="p" className="limitCharacters">
             {discription}
           </Typography>
         </CardContent>
       </CardActionArea>
+      <CreateButton color="primary" text="Add to cart" href="#"/>
     </Card>
   );
 }
