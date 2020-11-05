bd4ae4887c3dbd33a351686bbbee2fb0f15f1c09
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-05 18:49:18 +0200

    add review order page

---
+++ src/pages/ReviewOrder/ReviewOrder.js
@@ -0,0 +1,19 @@
+import './ReviewOrder.scss';
+// import Cart from '../../containers/Cart/Cart';
+// import  MediaControlCard from '../../components/MediaControlCard/MediaControlCard';
+import React, { Component } from 'react';
+class ReviewOrder extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <React.Fragment>
+                {/* <MediaControlCard
+                 title={title}
+                 img={img}
+                />*/}
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default ReviewOrder;
