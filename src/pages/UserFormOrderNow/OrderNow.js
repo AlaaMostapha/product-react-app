e5320759fc0795bcfbd1de4ce32c0a4d935f5fb7
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-05 18:48:32 +0200

    add order now page

---
+++ src/pages/UserFormOrderNow/OrderNow.js
@@ -0,0 +1,11 @@
+import './Order.scss'
+import React from 'react';
+
+export default function OrderNow(props){
+    return(
+        <div>
+            {/* formik */}
+            order now
+        </div>
+    )
+}
