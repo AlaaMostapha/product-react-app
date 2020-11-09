ddb97e37ac6eb3426160773652e63891b8a05d70
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-09 16:14:00 +0200

    working on quantity component

--- src/components/quantity/quantity.js
+++ src/components/quantity/quantity.js
@@ -1,57 +1,61 @@
 import './quantity.scss';
 import React,{ useState, useEffect }  from 'react';
-// import {connect} from 'react-redux';
+import {connect} from 'react-redux';
+import * as actions from '../../redux/actions/actions';
 
-
-function Quantity ({onChangeQuantity,quantity}) {
+function Quantity (props) {
     const inputRef=React.useRef(0);
     //set default value of input value with quantity from another pages
-    const [inputValue, setinputValue] = useState(quantity);
+    console.log(props)
+    // props.quantityValue=props.quantity
+    // console.log(props.quantityValue)
+    const [inputValue, setinputValue] = useState(props.quantity);
     useEffect(() => {
-        // Update the document title using the browser API
-        // console.log(inputRef)
-        // console.log(inputRef.current.value)
-        // console.log(onChangeQuantity(inputValue))
-        // console.log(props)
-        //   props.onChangeQuantity("x")
-        // console.log(quantity)
+        console.log(inputRef.current.value)
     });
    const decreaseValue=()=>{
-      
-        if(inputValue<=0){setinputValue(0)}else{ setinputValue(inputValue - 1)}
-
-         console.log(onChangeQuantity(inputValue))
+    //    if(inputValue<=0){
+    //          setinputValue(0)
+    //    }else{
+    //        setinputValue(inputValue-1)
+    //    }
+    //     console.log(props.onChangeQuantity(inputValue))
+       props.decrementQuantity(props.quantity)
     }
     const increaseValue=()=>{
-        
-         setinputValue(inputValue + 1)
-        //  console.log(inputValue)
-        //  quantity=inputValue;
-        //  console.log(quantity)
-         console.log(onChangeQuantity(inputValue))
+    //    setinputValue(inputValue+1)
+    //     console.log(props.onChangeQuantity(inputValue))
+        props.incrementQuantity(props.quantity)
     }
     const handleChange = (event) =>{
         // console.log(props)
-        setinputValue(event.target.value);
-      console.log(onChangeQuantity(inputValue))
+        // setinputValue(event.target.value);
+    //   console.log(onChangeQuantity(inputValue))
     }
 
     return ( 
         <div className="quantity">
             <div className="value-button" id="decrease" onClick={decreaseValue} value="Decrease Value">-</div>
-            <input type="number" id="number" value={inputValue} ref={inputRef}  onChange={handleChange}/>
+            <input type="number" id="number" value={(props.quantityValue)?props.quantityValue:inputValue} ref={inputRef}  onChange={handleChange}/>
             <div className="value-button" id="increase" onClick={increaseValue} value="Increase Value">+</div>
         </div> 
     );   
 }
  
-export default Quantity;
-// function mapStateToProps(state){
-// //   console.log(state)
-//   // if(state.cartReducer.newItemData.newItem)console.log(state.cartReducer.newItemData.newItem)
-//   //  console.log(state.productsReducer)
-//   return{
-//     // quantity:state.cartReducer.quantity,
-//   }
-// }
-// export default connect(mapStateToProps,null)(Quantity); 
+
+
+function mapDispatchToProps(dispatch){
+  return{
+    initializeQuantity:(quantity)=>dispatch(actions.initializeQuantity(quantity)),
+    decrementQuantity: (quantity)=>dispatch(actions.decrementQuantity(quantity)),
+    incrementQuantity:(quantity)=>dispatch(actions.incrementQuantity(quantity)),
+  }
+} 
+function mapStateToProps(state){
+  console.log(state)
+  //  console.log(state.productsReducer)
+  return{
+    quantityValue:state.cartReducer.quantityValue
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Quantity); 
