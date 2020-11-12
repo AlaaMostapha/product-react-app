f2abdc881a31be98320575a5e00406576be5116d
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-12 14:12:18 +0200

    check from details component if item in cart or not to return its quantity

--- src/pages/productDetails/productDetails.js
+++ src/pages/productDetails/productDetails.js
@@ -6,63 +6,65 @@ import * as actions from '../../redux/actions/actions';
 import LoadingIndicator from '../../components/LoadingIndicator/LoadingIndicator';
 import Quantity from '../../components/quantity/quantity';
 class ProductDetails extends Component {
-  state={
-    clicked:false
-  }
   componentDidUpdate(){
-     console.log(this.props.singleProduct.quantity)
+    const{cart,singleProduct}=this.props
+    //check if this item is in cart return it from cart with it's quantity
+    const  product = cart.find(product=>product.id===singleProduct.id)
+    if(product){
+        console.log('cart has this item')
+        this.props.showSingleProduct(product)
+    }else{
+        this.props.showSingleProduct(singleProduct)
+    }
+    console.log(singleProduct)
   }
-   componentDidMount(){  
-      const {productId}=this.props.match.params
-      console.log(productId);
-      this.props.getSingleProduct(productId)
-      // console.log(this.props.singleProduct.quantity)
-    }
-    addItem=(singleProduct)=>{
-      console.log(this.props.addItemInCart(singleProduct))
-      this.setState({
-        clicked:true
-      })
-    }
-    state = {  }
-    render() { 
-       const{singleProduct}=this.props;
-        return (  
-             <div >
-              
-                {(singleProduct)?
-                <div style={{display:"flex",textAlign:"left"}}>
-                  <img src={singleProduct.image} alt={singleProduct.alt} style={{height:"250px",margin:"10px"}}/>
-                  <div>
-                    <h3>{singleProduct.title}</h3>
-                    <p>{singleProduct.description}</p>
-                    <h4>{singleProduct.price}</h4>
-                    {/* <CreateButton color="primary" text="Add to cart" href="#"/> */}
-                    {console.log(singleProduct.quantity)}
-                    {(this.state.clicked)?<Quantity item={singleProduct}/>:
-                      // <button onClick={()=>this.addItem(singleProduct)}>Add to cart</button>
-                     <CreateButton color="primary" text="Add to cart"onClick={()=>this.addItem(singleProduct)}/>
-                     }
-                  </div>
-                </div>:<LoadingIndicator/>}
+  componentDidMount(){  
+    const {productId}=this.props.match.params
+    // request item from api
+    this.props.getSingleProduct(productId)
+  }
+
+  addItem=(singleProduct)=>{
+    //add item to cart
+    console.log(this.props.addItemInCart(singleProduct))
+  }
+  render() { 
+    const{singleProduct}=this.props;
+    return (  
+      <div>
+        {(singleProduct)?
+          <div style={{display:"flex",textAlign:"left"}}>
+            <img src={singleProduct.image} alt={singleProduct.alt} style={{height:"250px",margin:"10px"}}/>
+            <div>
+              <h3>{singleProduct.title}</h3>
+              <p>{singleProduct.description}</p>
+              <h4>{singleProduct.price}</h4>
+              {/* <CreateButton color="primary" text="Add to cart" href="#"/> */}
+              {/* {console.log(singleProduct.quantity)} */}
+              {(singleProduct.quantity)?<Quantity item={singleProduct}/>:
+              <CreateButton color="primary" text="Add to cart"onClick={()=>this.addItem(singleProduct)}/>
+              }
             </div>
-        );
-    }
+          </div>
+          :<LoadingIndicator/>}
+      </div>
+    );
+  }
 }
 
 // export default productDetails;
 function mapStateToProps(state){
   console.log(state)
-   console.log(state.singleProductReducer)
   return{
     singleProduct:state.singleProductReducer.singleProduct,
+    cart:state.cartReducer.cart,
   }
 }
 function mapDispatchToProps(dispatch){
   return{
-   getProducts : ()=>dispatch(actions.getProducts()),
-   getSingleProduct :(id)=>dispatch(actions.getSingleProduct(id)),
+    getSingleProduct :(id)=>dispatch(actions.getSingleProduct(id)),
     addItemInCart :(item)=>dispatch(actions.addItemInCart(item)),
+    showSingleProduct: (item)=>dispatch(actions.showSingleProduct(item))
   }
 } 
 export default connect(mapStateToProps,mapDispatchToProps)(ProductDetails); 
