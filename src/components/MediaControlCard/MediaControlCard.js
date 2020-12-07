c0625b2dd6ba9fee9aa2fbf0255a3b2ac801bbc7
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-07 15:31:06 +0200

    change in card of material ui design

--- src/components/MediaControlCard/MediaControlCard.js
+++ src/components/MediaControlCard/MediaControlCard.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useStyles } from "./style";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  CardActionArea,
+} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
 
@@ -15,27 +19,28 @@ export default function MediaControlCard({
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      {img ? (
-        <CardMedia
-          className={classes.cover}
-          image={img}
-          title="Live from space album cover"
-        />
-      ) : (
-        <LoadingIndicator />
-      )}
-      <div>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
-            {title}
-          </Typography>
-
+      <CardActionArea>
+        {img ? (
+          <CardMedia
+            className={classes.cover}
+            image={img}
+            title="Live from space album cover"
+          />
+        ) : (
+          <LoadingIndicator />
+        )}
+      </CardActionArea>
+      <CardContent className={classes.content}>
+        <Typography component="h5" variant="h5">
+          {title}
+        </Typography>
+        <CardActions>
           <div style={{ textAlign: "center", display: "flex" }}>
             {quantityComp ? quantityComp : ""}
             {{ deleteBtn } ? deleteBtn : ""}
           </div>
-        </CardContent>
-      </div>
+        </CardActions>
+      </CardContent>
     </Card>
   );
 }
