105685ac4e2b1d26abaf75a4991b8b644418a9ce
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-07 12:15:48 +0200

    change cart from class comp to func comp

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,32 +1,28 @@
 import MediaControlCard from "../../components/MediaControlCard/MediaControlCard";
-import React, { Component } from "react";
+import React from "react";
 import CreateButton from "../../components/Btn/Btn";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Quantity from "../../components/quantity/quantity";
 
-class Cart extends Component {
-  state = {};
-  render() {
-    const { title, img, onDelete, item } = this.props;
-
-    return (
-      <React.Fragment>
-        <MediaControlCard
-          className="carte"
-          title={title}
-          img={img}
-          quantityComp={<Quantity item={item} />}
-          deleteBtn={
-            <CreateButton
-              color="secondary"
-              text={<DeleteIcon />}
-              onClick={onDelete}
-            />
-          }
-        />
-      </React.Fragment>
-    );
-  }
+function Cart(props) {
+  const { title, img, onDelete, item } = props;
+  return (
+    <React.Fragment>
+      <MediaControlCard
+        className="carte"
+        title={title}
+        img={img}
+        quantityComp={<Quantity item={item} />}
+        deleteBtn={
+          <CreateButton
+            color="secondary"
+            text={<DeleteIcon />}
+            onClick={onDelete}
+          />
+        }
+      />
+    </React.Fragment>
+  );
 }
 
 export default Cart;
