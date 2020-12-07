a1dc8aa6bc03623b907852db2fd3c1cf65081fb7
AlaaMostapha <alaamoustapha21@gmail.com>
2020-12-07 14:53:03 +0200

    change reviewOrder page from class comp to funct. comp

--- src/pages/ReviewOrder/ReviewOrder.js
+++ src/pages/ReviewOrder/ReviewOrder.js
@@ -1,6 +1,6 @@
 import "./ReviewOrder.scss";
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Container from "@material-ui/core/Container";
 import LoadingIndicator from "../../components/LoadingIndicator/LoadingIndicator";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -9,164 +9,95 @@ import CreateButton from "../../components/Btn/Btn";
 import * as cartActions from "../../redux/actions/cart";
 import BasicTable from "../../components/table/table";
 import { TableCell, TableRow } from "@material-ui/core";
-class ReviewOrder extends Component {
+function ReviewOrder(props) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cartReducer.cart);
+  const cartLoader = useSelector((state) => state.cartReducer.cartLoader);
   //each product display with quantity and delete btn //rest unit price and total price
-  constructor(props) {
-    super(props);
-    this.carttotal = 0;
-  }
-  componentDidMount() {
-    console.log(this.props);
-  }
-  calculatTotalPrice() {
-    console.log(this.props.cart);
-    const cartTotal = this.props.cart
-      .reduce(function (accumlator, product) {
-        return accumlator + product.price * product.quantity;
-      }, 0)
-      .toFixed(3);
-    console.log(cartTotal);
-    return cartTotal;
-  }
-  removeItemFromCart = (product) => {
-    console.log("item remove");
-    this.total = 0;
-    this.props.deleteItemInCart(product);
+  const [carttotal, setcarttotal] = useState(0);
+
+  useEffect(() => {
+    setcarttotal(
+      cart
+        .reduce(function (accumlator, product) {
+          return accumlator + product.price * product.quantity;
+        }, 0)
+        .toFixed(3)
+    );
+  }, [cart]);
+  const removeItemFromCart = (product) => {
+    dispatch(cartActions.deleteItemInCart(product));
   };
-  createList() {
-    const { cart } = this.props;
-    if (cart) {
-      console.log("items in cart");
-      return cart.map((product) => {
-        return (
-          <TableRow key={product.id}>
-            <TableCell component="th" scope="row" display="flex">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="reviewPageImg"
-                  />
-                  {product.title}
-                </div>
-                <Quantity item={product} />
-              </div>
-            </TableCell>
-            <TableCell>{product.price}</TableCell>
-            <TableCell>
-              {(product.price * product.quantity).toFixed(3)}
-            </TableCell>
-            <TableCell>
-              <CreateButton
-                color="secondary"
-                text={<DeleteIcon />}
-                onClick={() => this.removeItemFromCart(product)}
-              />
-            </TableCell>
-          </TableRow>
-        );
-      });
-    } else {
-      console.log("no items in cart");
-      return <div>No items</div>;
-    }
-  }
-  redirectToUserForm = () => {
-    return this.props.history.push(`/OrderNow`);
+  const redirectToUserForm = () => {
+    return props.history.push(`/OrderNow`);
   };
-  tableHeadings = ["Item", "Unit Price", "Total Price", "Delete"];
-  render() {
-    const { cartLoader } = this.props;
-    return (
-      <Container maxWidth="lg" className="ProductListContainer">
-        <h2 className="text-center">Review Your Order</h2>
-        {cartLoader ? (
-          <LoadingIndicator />
-        ) : (
-          <BasicTable
-            tableHeadings={this.tableHeadings}
-            arrayItems={this.props.cart.map((product, index) => (
-              <>
-                <TableRow key={product.name}>
-                  <TableCell component="th" scope="row" display="flex">
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <img
-                          src={product.image}
-                          alt={product.title}
-                          className="reviewPageImg"
-                        />
-                        {product.title}
-                      </div>
-                      <Quantity item={product} />
+  const tableHeadings = ["Item", "Unit Price", "Total Price", "Delete"];
+
+  return (
+    <Container maxWidth="lg" className="ProductListContainer">
+      <h2 className="text-center">Review Your Order</h2>
+      {cartLoader ? (
+        <LoadingIndicator />
+      ) : (
+        <BasicTable
+          tableHeadings={tableHeadings}
+          arrayItems={cart.map((product, index) => (
+            <>
+              <TableRow key={product.name}>
+                <TableCell component="th" scope="row" display="flex">
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img
+                        src={product.image}
+                        alt={product.title}
+                        className="reviewPageImg"
+                      />
+                      {product.title}
                     </div>
-                  </TableCell>
-                  <TableCell>{product.price}</TableCell>
+                    <Quantity item={product} />
+                  </div>
+                </TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>
+                  {(product.price * product.quantity).toFixed(3)}
+                </TableCell>
+                <TableCell>
+                  <CreateButton
+                    color="secondary"
+                    text={<DeleteIcon />}
+                    onClick={() => removeItemFromCart(product)}
+                  />
+                </TableCell>
+              </TableRow>
+              {index === cart.length - 1 && (
+                <TableRow>
                   <TableCell>
-                    {(product.price * product.quantity).toFixed(3)}
+                    <b>Total</b>
                   </TableCell>
-                  <TableCell>
-                    <CreateButton
-                      color="secondary"
-                      text={<DeleteIcon />}
-                      onClick={() => this.removeItemFromCart(product)}
-                    />
+                  <TableCell colspan={3} style={{ textAlign: "center" }}>
+                    <b>{carttotal}</b>
                   </TableCell>
                 </TableRow>
-                {index === this.props.cart.length - 1 && (
-                  <TableRow>
-                    <TableCell>
-                      <b>Total</b>
-                    </TableCell>
-                    <TableCell colspan={3} style={{ textAlign: "center" }}>
-                      <b>{this.calculatTotalPrice()}</b>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </>
-            ))}
-          />
-        )}
-        <div style={{ textAlign: "center" }}>
-          <CreateButton
-            color="primary"
-            text="Order Now"
-            onClick={this.redirectToUserForm}
-          />
-        </div>
-      </Container>
-    );
-  }
+              )}
+            </>
+          ))}
+        />
+      )}
+      <div style={{ textAlign: "center" }}>
+        <CreateButton
+          color="primary"
+          text="Order Now"
+          onClick={redirectToUserForm}
+        />
+      </div>
+    </Container>
+  );
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    deleteItemInCart: (product) =>
-      dispatch(cartActions.deleteItemInCart(product)),
-  };
-}
-function mapStateToProps(state) {
-  console.log(state);
-  //  console.log(state.productsReducer)
-  return {
-    cart: state.cartReducer.cart,
-    cartLoader: state.cartReducer.cartLoader,
-  };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(ReviewOrder);
-
-// first quantity come from store to display in review OrderNow
-// after that it change in page
+export default ReviewOrder;
